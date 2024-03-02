055b204cb4b8978d9a2e1afb91abd742fd02fd6c
Vinit Kumar <63940739+router19@users.noreply.github.com>
2024-03-02 09:45:14 +0530

    Time: 0 ms (100%), Space: 42.4 MB (82.53%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -4,18 +4,12 @@ class Solution {
         generateSubsets(nums, 0, new ArrayList<>(), result);
         return result;
     }
-     private void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
-        if (index == nums.length) {
-            result.add(new ArrayList<>(current));
-            return;
+     private void generateSubsets(int[] nums, int start, List<Integer> current, List<List<Integer>> res) {
+        res.add(new ArrayList<>(current));
+        for(int i = start;i < nums.length; i++ ){
+            current.add(nums[i]);
+            generateSubsets(nums,i+1, current, res);
+            current.remove(current.size() -1);
         }
-
-        // Include the current element
-        current.add(nums[index]);
-        generateSubsets(nums, index + 1, current, result);
-
-        // Exclude the current element
-        current.remove(current.size() - 1);
-        generateSubsets(nums, index + 1, current, result);
     }
 }
